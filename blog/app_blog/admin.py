9160dff142333423d15867a6e0870beaa6ceb0a1
Илья Горчаков <brainburst54@gmail.com>
2022-06-21 14:21:35 +0300

    Обновление app_shop.views для отображения списка и деталей поста. Добавление&обновление url'ов

--- blog/app_blog/admin.py
+++ blog/app_blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from app_blog import models
+from . import models
 
 
 @admin.register(models.Post)
--- blog/app_blog/views.py
+++ blog/app_blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
-# Create your views here.
+from . import models
+
+
+class PostList(ListView):
+    queryset = models.Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post,list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(models.Post, slug=post, status='published',
+                             published__year=year, published__month=month, published__day=day)
+    return render(request, 'blog/post/detail.html', {'post': post})
