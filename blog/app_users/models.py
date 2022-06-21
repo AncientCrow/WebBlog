d23ba3fe5e765f066acc7c144785d1a088b82e8c
Илья Горчаков <brainburst54@gmail.com>
2022-06-21 11:09:28 +0300

    Добавлены модели Post и Profile

--- blog/app_users/models.py
+++ blog/app_users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name="user")
+    city = models.CharField(max_length=36, null=True, verbose_name="city")
+    about = models.TextField(max_length=10000, null=True, verbose_name="about")
+    birthday = models.DateField(verbose_name="birthday")
+    verify = models.BooleanField(default=False, verbose_name="verification")
+
+    class Meta:
+        permissions = (
+            ("verified", "Верифицирован"),
+        )
+        verbose_name = "profile"
+        verbose_name_plural = "profiles"
+
+    def get_absolute_url(self):
+        return f"/user/{self.id}"
+
+
+class ProfileIcon(models.Model):
+    icon = models.ImageField(upload_to="users")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "user_icon"
