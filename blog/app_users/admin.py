7d55c425b493dfa1537da82c136cb4efc97b9d4a
Илья Горчаков <brainburst54@gmail.com>
2022-06-22 15:32:34 +0300

    Добавление возможности просмотра пользователя и его статей

--- blog/app_users/admin.py
+++ blog/app_users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user', )
