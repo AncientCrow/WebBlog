641af64568f7d704e2bef6779214e3a705118ae6
Илья Горчаков <brainburst54@gmail.com>
2022-06-25 21:11:09 +0300

    настроена система подписок без ajax и динамического добавления

--- blog/app_users/models.py
+++ blog/app_users/models.py
@@ -1,17 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import User
-
-
-class Follow(models.Model):
-    follower = models.ForeignKey(User, related_name='rel_from_set', on_delete=models.CASCADE)
-    followed = models.ForeignKey(User, related_name='rel_to_set', on_delete=models.CASCADE)
-    follow_date = models.DateTimeField(auto_now_add=True, db_index=True)
-
-    class Meta:
-        ordering = ('-follow_date',)
-
-    def __str__(self):
-        return '{} follows {}'.format(self.follower, self.followed)
+from django.db import models
 
 
 class Profile(models.Model):
@@ -19,6 +7,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='user')
     about = models.TextField(max_length=10000, null=True, verbose_name='about')
     birthday = models.DateField(verbose_name='birthday')
+    followers = models.ManyToManyField(User, related_name='following',
+                                       blank=True)
 
     class Meta:
         verbose_name = 'profile'
@@ -27,11 +17,5 @@ class Profile(models.Model):
     def get_absolute_url(self):
         return f'/user/{self.id}'
 
-
-# модель пользователя не определена, требуется динамическое добавление
-User.add_to_class('following',
-                  models.ManyToManyField('self',
-                                         through=Follow,
-                                         related_name='followers',
-                                         symmetrical=False)
-                  )
+    def __str__(self):
+        return str(self.user.username)
--- blog/app_users/urls.py
+++ blog/app_users/urls.py
@@ -5,8 +5,10 @@ app_name = 'users'
 
 urlpatterns = [
     path('', views.UserList.as_view(), name='user_list'),
+
     path('filter/<int:pk>/', views.UsersFilter.as_view(), name='users_filter'),
     path('user/<username>/', views.UserDetail.as_view(), name='user_detail'),
+    path('follow/', views.user_follow, name='user_follow'),
     path("registration/", views.RegistrationPage.as_view(), name="registration"),
     path("login/", views.LoginPage.as_view(), name="login"),
     path("logout/", views.LogoutPage.as_view(), name="logout")
