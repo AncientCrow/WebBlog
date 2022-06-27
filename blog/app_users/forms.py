6df43e9590ae9f3e0afaa4a2ec9a6a1503db27e3
Илья Горчаков <brainburst54@gmail.com>
2022-06-27 16:38:06 +0300

    тесты формы регистрации

--- blog/app_users/forms.py
+++ blog/app_users/forms.py
@@ -8,10 +8,9 @@ from django.contrib.auth.models import User
 class RegistrationForm(forms.Form):
     icon = forms.ImageField(required=False, label='Изображение профиля')
     username = forms.CharField(max_length=25, label='Логин')
-    password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput, label='Пароль')
     password_confirm = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
-    about = forms.CharField(max_length=10000, required=False)
-
+    about = forms.CharField(max_length=10000, required=False, label='О себе')
     start_year = datetime.date.today().year - 120
     end_year = datetime.date.today().year - 16
     birthday = forms.DateField(
